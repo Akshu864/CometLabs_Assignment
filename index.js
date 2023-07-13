ad5d7f380900b7320b748e57b2279a8e25c8ec9f
“Akshu864” <bhardwahjakshay14@gmail.com>
2023-07-13 22:51:05 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-mongoose.connect("https://github.com/Akshu864/CometLabs_Assignment.git",{useNewUrlParser: true})
+mongoose.connect("mongodb+srv://Akshu12:Akshay123@cluster0.eqljz.mongodb.net/MyAssignment_1",{useNewUrlParser: true})
 
 .then(()=>{
     console.log("mongodb is connected")
