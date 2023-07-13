5ea54681338f0bfc3ac329134b83c9a17e694ce5
“Akshu864” <bhardwahjakshay14@gmail.com>
2023-07-13 23:39:06 +0530

    vercel

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -28,15 +28,23 @@ const authMiddleware=require('../middleware/authMiddleware')
 // router.post('/question/:questionId/solution', authMiddleware, questionController.checkSolution);
 
 // Routes for user-related APIs
-router.post('/api/signup', userController.signUp);
-router.post('/api/login', userController.login);
+// const express = require('express');
+// // const router = express.Router();
+// const userController = require('../controller/userController');
+// const questionController = require('../controller/questionController');
+// const authMiddleware = require('../middleware/authMiddleware');
 
-// Routes for question-related APIs
-router.post('/api/question', authMiddleware, questionController.addQuestion);
-router.put('/api/question/:id', authMiddleware, questionController.editQuestion);
-router.delete('/api/question/:id', authMiddleware, questionController.deleteQuestion);
-router.post('/api/question/:questionId/testcase', authMiddleware, questionController.addTestCase);
-router.post('/api/question/:questionId/solution', authMiddleware, questionController.checkSolution);
+router.post('/signup', userController.signUp);
+router.post('/login', userController.login);
+
+router.post('/question', authMiddleware, questionController.addQuestion);
+router.put('/question/:id', authMiddleware, questionController.editQuestion);
+router.delete('/question/:id', authMiddleware, questionController.deleteQuestion);
+router.post('/question/:questionId/testcase', authMiddleware, questionController.addTestCase);
+router.post('/question/:questionId/solution', authMiddleware, questionController.checkSolution);
+
+module.exports = router;
+
 
 module.exports = router;
 
